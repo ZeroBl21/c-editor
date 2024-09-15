9cf125a2d9ef1fde28efcf6dcac483b2df726074
Darwin Perdomo <ZeroBl@hotmail.com>
2024-09-15 12:34:52 -0400

    feat(row-operations): Add decimal numbers highlight

--- src/row-operations.c
+++ src/row-operations.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -154,15 +155,28 @@ void editor_row_del_char(EditorRow *row, int at) {
 }
 
 // Syntax Hightlight
+int is_separator(int c) {
+  return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
 
 void editor_update_syntax(EditorRow *row) {
   row->hl = realloc(row->hl, row->r_size);
   memset(row->hl, HL_NORMAL, row->r_size);
 
+  int prev_sep = 1;
+
   for (int i = 0; i < row->r_size; i++) {
-    if (isdigit(row->r_chars[i])) {
+    char c = row->r_chars[i];
+    uint8_t prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) ||
+        (c == '.' && prev_hl == HL_NUMBER)) {
       row->hl[i] = HL_NUMBER;
+      prev_sep = 0;
+      continue;
     }
+
+    prev_sep = is_separator(c);
   }
 }
 
